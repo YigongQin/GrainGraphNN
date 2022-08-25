d1d8b09d8a2f2200873a23303ed84ef10f910165
Yigong Qin <yigongqin@Yigongs-MBP.lan>
2022-08-25 14:46:40 -0500

    graph

--- data/graph_datastruct.py
+++ data/graph_datastruct.py
@@ -53,30 +53,34 @@ def hexagonal_lattice(dx=0.05, noise=0.0001):
 
         
 class graph:
-    def __init__(self, size, density = 0.05, noise =0.00005):
+    def __init__(self, size, density = 0.05, noise =0.00005, randInit = True):
 
         self.imagesize = size
-        self.vertices = [] 
-        self.edges = []
-        self.regions = []
-        self.region_orientation = []
+        self.vertices = [] ## vertices coordinates
+        self.edges = []  ## index linkage
+        self.edge_colors = []  ## defined by region orientation 
+        self.regions = [] ## index group
+        self.region_colors = []  ##color
+       # self.region_coors = [] ## region corner coordinates
         self.density = density
         self.noise = noise
         self.alpha_field = None
-        vor, self.region_coors = self.inbound_voronoi()
-        self.plot_polygons(self.region_coors, pic_size=self.imagesize)
         
-    def inbound_voronoi(self):
+        if randInit:
+            self.inbound_random_voronoi()    
+            self.plot_polygons(pic_size=self.imagesize)
+        
+    def inbound_random_voronoi(self):
 
         mirrored_seeds, seeds = hexagonal_lattice(dx=self.density, noise = self.noise)
         vor = Voronoi(mirrored_seeds)     
     
         regions = []
-        reordered_regions = []
-        vertices = []
+       # reordered_regions = []
+       # vertices = []
         vert_map = {}
         vert_count = 0
-        edges = []
+       # edges = []
         
         for region in vor.regions:
             flag = True
@@ -91,48 +95,59 @@ class graph:
                         flag = False
                         break
             if region != [] and flag:
+                
+                '''
+                valid region propertities
+                '''
+                
                 regions.append(region)
                 reordered_region = []
+                orientation = random.randint(0,255)
+                self.region_colors.append(orientation)
+                
                 for index in region:
                     point = tuple(vor.vertices[index])
                     if point not in vert_map:
-                        vertices.append(point)
+                        self.vertices.append(point)
                         vert_map[point] = vert_count
                         reordered_region.append(vert_count)
                         vert_count += 1
                     else:
                         reordered_region.append(vert_map[point])
-                for i in range(len(region)):
+                for i in range(len(reordered_region)):
                     cur = vor.vertices[region[i]]
                     nxt = vor.vertices[region[i+1]] if i<len(region)-1 else vor.vertices[region[0]]
-                    edges.append([vert_map[tuple(cur)], vert_map[tuple(nxt)]])
-                reordered_regions.append(reordered_region)
+                    self.edges.append([vert_map[tuple(cur)], vert_map[tuple(nxt)]])
+                    self.edge_colors.append(orientation)
+                self.regions.append(reordered_region)
                     
+
+        self.vertices = np.array(self.vertices)
+
+    
         vor.filtered_points = seeds
         vor.filtered_regions = regions
-        self.regions = reordered_regions
-        self.vertices = np.array(vertices)
-        self.edges = edges
+    
+    
+    def reconstruct_polygons(self):
         
-        polygon_coors = []
-        for reg in self.regions:
-            polygon = self.vertices[reg]
-            polygon_coors.append(polygon)
+        ## reconstruct with modified 
+        
+        return
     
     
-        return vor, polygon_coors
-    
-    
-    def plot_polygons(self,polygons, pic_size):
+    def plot_polygons(self, pic_size):
 
         image = Image.new("L", (pic_size[0], pic_size[1]))       
         draw = ImageDraw.Draw(image)
           
         # Add polygons 
-        for poly in polygons:
+        for i in range(len(self.regions)):
+            reg = self.regions[i]
+            poly = self.vertices[reg]
+            orientation = self.region_colors[i]
             p = []
-            orientation = random.randint(0,255)
-            self.region_orientation.append(orientation)
+
             poly = np.asarray(poly*pic_size[0], dtype=int) 
             for i in range(poly.shape[0]):
                 p.append(tuple(poly[i]))
@@ -175,6 +190,32 @@ class graph:
 
 
 
-g1 = graph(size = (1000, 1000), density = 0.2, noise=0.001)   
-g1.show_data_struct()     
+#g1 = graph(size = (1000, 1000), density = 0.2, noise=0.001)   
+#g1.show_data_struct()     
 
+
+import pickle
+
+
+with open('graph_data.pkl', 'wb') as outp:
+    
+    for i in range(5):
+        g1 = graph(size = (1000, 1000), density = 0.2, noise=0.001)  
+        pickle.dump(g1, outp, pickle.HIGHEST_PROTOCOL)
+    del g1
+
+
+with open('graph_data.pkl', 'rb') as inp:
+      
+   while True:
+        try:
+            g1 = pickle.load(inp)
+            g1.show_data_struct()
+        except EOFError:
+            break
+ 
+
+    
+    
+    
+    

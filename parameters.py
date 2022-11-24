0d8d4246c26d3e65e10f0b926444c94c45a93c36
Yigong Qin <yigongqin@Yigongs-MBP.lan>
2022-11-24 15:25:10 -0600

    param

--- parameters.py
+++ parameters.py
@@ -10,62 +10,74 @@ class Param(Updater):
 
 
 
-def hyperparam(mode, all_id):
+def hyperparam(mode, model_id):
 
 
-	'''
-    user defined
-	'''
+    Ct = 1
 
-	Ct = 1   
-	Cl = 1   
-	all_frames = 600*Ct + 1
-	G = 32
+    all_frames = 20*Ct + 1
 
-	'''
-    hyperparameter grid
-	'''
-	frames_pool=[20,24,30]
-	learning_rate_pool=[25e-4, 50e-4, 100e-4]
-	layers_pool=[3,4,5]
-	hidden_dim_pool = [16, 24, 32]
-	out_win_pool = [4, 5, 6] 
+        
+    hp_grid = {'frames':[20, 24, 30], \
+               'lr':[25e-4, 50e-4, 100e-4, 200e-4], \
+               'layer':[2, 3, 4],\
+               'hidden':[16, 24, 32],\
+               'weight':[5, 10, 20, 40]}
+        
+    
+    hp_size = [(len(v),k) for k, v in hp_grid.items()]
+    hp_order = [4, 1, 2, 3, 0]
+    hp_size = [hp_size[i] for i in hp_order]
 
-	frames_id = all_id//81
-	lr_id = (all_id%81)//27
-	layers_id = (all_id%27)//9
-	hd_id = (all_id%9)//3
-	owin_id = all_id%3
+    param_dict = {}
+    prev_dim = 1
+    
+    for grid_dim, param in hp_size:
+        
+        cur_dim = prev_dim*grid_dim
+        
+        
+        param_idx = (model_id%cur_dim)//prev_dim
+        param_dict.update({param:hp_grid[param][param_idx]})
+        
+        prev_dim = cur_dim
+        
+        """
+        frames_id = all_id//81
+        lr_id = (all_id%81)//27
+        layers_id = (all_id%27)//9
+        hd_id = (all_id%9)//3
+        owin_id = all_id%3
+        """
+        
+       
+       
+    param_dict['frames'] = param_dict['frames']*Ct+1
 
 
-	frames = frames_pool[frames_id]*Ct+1
-	learning_rate = learning_rate_pool[lr_id]
-	layers = layers_pool[layers_id]
-	hidden_dim=hidden_dim_pool[hd_id]
-	out_win = out_win_pool[owin_id]
 
+    out_win = 1
+    if mode=='train' or mode=='test':
+    	  window = out_win
+    	  pred_frames = param_dict['frames']-window
 
-	if mode=='train' or mode=='test':
-	  window = out_win
-	  pred_frames = frames-window
-
-	if mode=='ini':
-	  learning_rate *= 2
-	  window = 1
-	  pred_frames = out_win - 1
+    if mode=='ini':
+    	  param_dict['lr'] *= 2
+    	  window = 1
+    	  pred_frames = out_win - 1
 
 
 	
-	dt = Ct*1.0/(frames-1)
+    dt = Ct*1.0/(param_dict['frames']-1)
 
 
 
 
-	param_dict = {'all_frames':all_frames, 'frames':frames, 'window':window, 'out_win':1, 'pred_frames':pred_frames, 'dt':dt, \
-	             'layers':1, 'layer_size':32, 'kernel_size':(3,), 'lr':0.01, 'epoch':60, 'bias':True, 'model_list':[0]}
+    param_dict.update({'all_frames':all_frames, 'window':window, 'out_win':1, 'pred_frames':pred_frames, 'dt':dt, \
+	             'layers':1, 'layer_size':32, 'kernel_size':(3,), 'lr':0.01, 'epoch':60, 'bias':True, 'model_list':[0]})
 
 
-	return Param(param_dict)
+    return Param(param_dict)
 
 
 

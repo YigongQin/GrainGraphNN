36c3343005f0e28047dcd9668a3388ab81497bef
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-12-09 16:54:14 -0600

    bound

--- graph_datastruct.py
+++ graph_datastruct.py
@@ -1136,8 +1136,7 @@ class GrainHeterograph:
             self.additional_features['nxt'] = nxt.edge_index_dicts
             
             
-            assert np.all(self.target_dicts['joint']>-1) and (np.all(self.target_dicts['joint']<1))
-            assert np.all(self.target_dicts['grain']>-1) and (np.all(self.target_dicts['grain']<1))
+
             
             # check if the grain neighbor of the junction is the same
             for i in range(len(self.mask['joint'])):
@@ -1148,6 +1147,10 @@ class GrainHeterograph:
                         self.mask['joint'][i,0] = 0
                       #  print('not matched', i, self.vertex2joint[i])
             
+            assert np.all(self.mask['joint']*self.target_dicts['joint']>-1) \
+               and np.all(self.mask['joint']*self.target_dicts['joint']<1)
+            assert np.all(self.target_dicts['grain']>-1) and (np.all(self.target_dicts['grain']<1))
+
                                         
         """
             

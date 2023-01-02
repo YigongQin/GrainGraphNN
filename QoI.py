cb4d6c4cffb41613dfa8b538f50c772750b0bfc8
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2023-01-02 12:42:55 -0600

    metric

---
+++ QoI.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 13:50:04 2022
+
+@author: yigongqin
+"""
+
+
+        
+class QoI_trajectory:
+    def __init__(self, ):
+        
+        self.num_grains
+        
+    def plot(self):
+        
+        return
+    
+
+class data_analysis:
+    def __init__(self, data_list):
+        self.list = data_list
+        
+    def scale(self):
+        coeff = None

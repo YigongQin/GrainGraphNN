3f6221ec4162fe3fc99c5a9a027b49f93014c052
Yigong Qin <yigongqin@Yigongs-MBP.lan>
2023-02-07 22:28:09 -0600

    plot

--- visualization3D/grain.py
+++ visualization3D/grain.py
@@ -21,52 +21,48 @@ print("imageFilesOut = " + imageFilesOut)
 paraview.simple._DisableFirstRenderCameraReset()
 
 # create a new 'Legacy VTK Reader'
-testvtk = LegacyVTKReader(registrationName='test.vtk', FileNames=[datasetIn])
+seed20_G50_R50vtk = LegacyVTKReader(registrationName='test.vtk', FileNames=[datasetIn])
 
 # get active view
-
 renderView1 = GetActiveViewOrCreate('RenderView')
 
 # show data in view
-testvtkDisplay = Show(testvtk, renderView1, 'UniformGridRepresentation')
+seed20_G50_R50vtkDisplay = Show(seed20_G50_R50vtk, renderView1, 'UniformGridRepresentation')
 
 # trace defaults for the display properties.
-testvtkDisplay.Representation = 'Outline'
-testvtkDisplay.ColorArrayName = ['POINTS', '']
-testvtkDisplay.SelectTCoordArray = 'None'
-testvtkDisplay.SelectNormalArray = 'None'
-testvtkDisplay.SelectTangentArray = 'None'
-testvtkDisplay.OSPRayScaleArray = 'theta_z'
-testvtkDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
-testvtkDisplay.SelectOrientationVectors = 'None'
-testvtkDisplay.ScaleFactor = 2.0
-testvtkDisplay.SelectScaleArray = 'theta_z'
-testvtkDisplay.GlyphType = 'Arrow'
-testvtkDisplay.GlyphTableIndexArray = 'theta_z'
-testvtkDisplay.GaussianRadius = 0.1
-testvtkDisplay.SetScaleArray = ['POINTS', 'theta_z']
-testvtkDisplay.ScaleTransferFunction = 'PiecewiseFunction'
-testvtkDisplay.OpacityArray = ['POINTS', 'theta_z']
-testvtkDisplay.OpacityTransferFunction = 'PiecewiseFunction'
-testvtkDisplay.DataAxesGrid = 'GridAxesRepresentation'
-testvtkDisplay.PolarAxes = 'PolarAxesRepresentation'
-testvtkDisplay.ScalarOpacityUnitDistance = 0.1385685404161374
-testvtkDisplay.OpacityArrayName = ['POINTS', 'theta_z']
-testvtkDisplay.ColorArray2Name = ['POINTS', 'theta_z']
-testvtkDisplay.IsosurfaceValues = [42.154137273875]
-testvtkDisplay.SliceFunction = 'Plane'
-testvtkDisplay.Slice = 123
-testvtkDisplay.SelectInputVectors = [None, '']
-testvtkDisplay.WriteLog = ''
+seed20_G50_R50vtkDisplay.Representation = 'Outline'
+seed20_G50_R50vtkDisplay.ColorArrayName = ['POINTS', '']
+seed20_G50_R50vtkDisplay.SelectTCoordArray = 'None'
+seed20_G50_R50vtkDisplay.SelectNormalArray = 'None'
+seed20_G50_R50vtkDisplay.SelectTangentArray = 'None'
+seed20_G50_R50vtkDisplay.OSPRayScaleArray = 'theta_z'
+seed20_G50_R50vtkDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+seed20_G50_R50vtkDisplay.SelectOrientationVectors = 'None'
+seed20_G50_R50vtkDisplay.ScaleFactor = 2.0
+seed20_G50_R50vtkDisplay.SelectScaleArray = 'theta_z'
+seed20_G50_R50vtkDisplay.GlyphType = 'Arrow'
+seed20_G50_R50vtkDisplay.GlyphTableIndexArray = 'theta_z'
+seed20_G50_R50vtkDisplay.GaussianRadius = 0.1
+seed20_G50_R50vtkDisplay.SetScaleArray = ['POINTS', 'theta_z']
+seed20_G50_R50vtkDisplay.ScaleTransferFunction = 'PiecewiseFunction'
+seed20_G50_R50vtkDisplay.OpacityArray = ['POINTS', 'theta_z']
+seed20_G50_R50vtkDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+seed20_G50_R50vtkDisplay.DataAxesGrid = 'GridAxesRepresentation'
+seed20_G50_R50vtkDisplay.PolarAxes = 'PolarAxesRepresentation'
+seed20_G50_R50vtkDisplay.ScalarOpacityUnitDistance = 0.1385685404161374
+seed20_G50_R50vtkDisplay.OpacityArrayName = ['POINTS', 'theta_z']
+seed20_G50_R50vtkDisplay.IsosurfaceValues = [42.154137273875]
+seed20_G50_R50vtkDisplay.SliceFunction = 'Plane'
+seed20_G50_R50vtkDisplay.Slice = 123
 
 # init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
-testvtkDisplay.ScaleTransferFunction.Points = [0.75957012975, 0.0, 0.5, 0.0, 83.548704418, 1.0, 0.5, 0.0]
+seed20_G50_R50vtkDisplay.ScaleTransferFunction.Points = [0.75957012975, 0.0, 0.5, 0.0, 83.548704418, 1.0, 0.5, 0.0]
 
 # init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
-testvtkDisplay.OpacityTransferFunction.Points = [0.75957012975, 0.0, 0.5, 0.0, 83.548704418, 1.0, 0.5, 0.0]
+seed20_G50_R50vtkDisplay.OpacityTransferFunction.Points = [0.75957012975, 0.0, 0.5, 0.0, 83.548704418, 1.0, 0.5, 0.0]
 
 # init the 'Plane' selected for 'SliceFunction'
-testvtkDisplay.SliceFunction.Origin = [10.0, 10.0, 9.88]
+seed20_G50_R50vtkDisplay.SliceFunction.Origin = [10.0, 10.0, 9.88]
 
 # reset view to fit data
 renderView1.ResetCamera(False)
@@ -78,13 +74,13 @@ materialLibrary1 = GetMaterialLibrary()
 renderView1.Update()
 
 # set scalar coloring
-ColorBy(testvtkDisplay, ('POINTS', 'theta_z'))
+ColorBy(seed20_G50_R50vtkDisplay, ('POINTS', 'theta_z'))
 
 # rescale color and/or opacity maps used to include current data range
-testvtkDisplay.RescaleTransferFunctionToDataRange(True, False)
+seed20_G50_R50vtkDisplay.RescaleTransferFunctionToDataRange(True, False)
 
 # show color bar/color legend
-testvtkDisplay.SetScalarBarVisibility(renderView1, True)
+seed20_G50_R50vtkDisplay.SetScalarBarVisibility(renderView1, True)
 
 # get color transfer function/color map for 'theta_z'
 theta_zLUT = GetColorTransferFunction('theta_z')
@@ -92,11 +88,8 @@ theta_zLUT = GetColorTransferFunction('theta_z')
 # get opacity transfer function/opacity map for 'theta_z'
 theta_zPWF = GetOpacityTransferFunction('theta_z')
 
-# get 2D transfer function for 'theta_z'
-theta_zTF2D = GetTransferFunction2D('theta_z')
-
 # change representation type
-testvtkDisplay.SetRepresentationType('Surface')
+seed20_G50_R50vtkDisplay.SetRepresentationType('Surface')
 
 # Properties modified on renderView1
 renderView1.UseColorPaletteForBackground = 0
@@ -104,50 +97,34 @@ renderView1.UseColorPaletteForBackground = 0
 # Properties modified on renderView1
 renderView1.Background = [1.0, 1.0, 1.0]
 
-# Rescale transfer function
-theta_zLUT.RescaleTransferFunction(0.0, 90.0)
+# hide color bar/color legend
+seed20_G50_R50vtkDisplay.SetScalarBarVisibility(renderView1, False)
 
-# Rescale transfer function
-theta_zPWF.RescaleTransferFunction(0.0, 90.0)
-
-# Rescale 2D transfer function
-theta_zTF2D.RescaleTransferFunction(0.0, 90.0, 0.0, 1.0)
+# show color bar/color legend
+seed20_G50_R50vtkDisplay.SetScalarBarVisibility(renderView1, True)
 
 # get color legend/bar for theta_zLUT in view renderView1
 theta_zLUTColorBar = GetScalarBar(theta_zLUT, renderView1)
 
 # Properties modified on theta_zLUTColorBar
-theta_zLUTColorBar.UseCustomLabels = 1
-theta_zLUTColorBar.CustomLabels = [0.0, 30.0, 60.0, 60.0]
-theta_zLUTColorBar.AddRangeLabels = 0
-
-# Properties modified on theta_zLUTColorBar
 theta_zLUTColorBar.Title = '$\\theta_z$'
-theta_zLUTColorBar.CustomLabels = [0.0, 30.0, 60.0, 90.0]
-
-#================================================================
-# addendum: following script captures some of the application
-# state to faithfully reproduce the visualization during playback
-#================================================================
+theta_zLUTColorBar.UseCustomLabels = 1
+theta_zLUTColorBar.CustomLabels = [30.0, 60.0]
+theta_zLUTColorBar.AddRangeLabels = 0
+theta_zLUTColorBar.ScalarBarThickness = 32
 
 # get layout
 layout1 = GetLayout()
 
-#--------------------------------
-# saving layout sizes for layouts
-
 # layout/tab size in pixels
-layout1.SetSize(1626, 1378)
-
-#-----------------------------------
-# saving camera placements for views
+layout1.SetSize(1165, 804)
 
 # current camera placement for renderView1
-renderView1.CameraPosition = [-30.452731331138263, -10.370708981221844, 58.78254631264396]
-renderView1.CameraFocalPoint = [10.000000000000002, 10.000000000000002, 9.880000114440918]
-renderView1.CameraViewUp = [0.6395932442017959, 0.3601758751380124, 0.67911252450532]
+renderView1.CameraPosition = [-22.133058935489576, -10.64508851958588, 64.50687193299979]
+renderView1.CameraFocalPoint = [9.999999999999991, 9.999999999999998, 9.880000114440918]
+renderView1.CameraViewUp = [0.19918727129422006, 0.8721825671628037, 0.44679077932704053]
 renderView1.CameraParallelScale = 17.25150434777653
 
 # save screenshot
-SaveScreenshot(directoryOut+imageFilesOut+'.png', renderView1, ImageResolution=[1024, 1024])
+SaveScreenshot(directoryOut+imageFilesOut+'.png', renderView1, ImageResolution=[1165, 804])
 

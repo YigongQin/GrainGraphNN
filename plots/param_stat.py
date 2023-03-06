75836130025fe4d3b730ea6584f0f3e81bac2671
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2023-03-06 13:42:53 -0600

    plot

---
+++ plots/param_stat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 10:49:10 2023
+
+@author: yigongqin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 24})
+G_, R_ = [], []
+
+for seed in range(10000, 10100) :      
+    np.random.seed(seed)
+    G = np.random.random()*(10-0.5) + 0.5
+    R = np.random.random()*(2-0.2) + 0.2
+    G_.append(G)
+    R_.append(R)
+    
+fig, ax = plt.subplots(figsize=(4,4))
+ax.scatter(G_, R_)
+ax.set_xlabel(r'$G\ (K/\mu m)$')
+ax.set_ylabel(r'$R\ (m/s) $')

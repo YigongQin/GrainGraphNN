90ea783a7aea4e4ae954a09a38a4c3155a79beb9
Yigong Qin <yigongqin@Yigongs-MBP.lan>
2023-04-20 16:22:23 -0500

    without attention

--- periodconv.py
+++ periodconv.py
@@ -1,165 +1,240 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 20 14:47:33 2022
+import math
+from typing import Optional, Tuple, Union
 
-@author: yigongqin
-"""
-
-from typing import List, Optional, Tuple, Union
-
-import torch.nn.functional as F
 import torch
+import torch.nn.functional as F
 from torch import Tensor
-from torch.nn import LSTM
-from torch_sparse import SparseTensor, matmul
+from torch_sparse import SparseTensor
 
-from torch_geometric.nn.aggr import Aggregation, MultiAggregation
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.dense.linear import Linear
-from torch_geometric.typing import Adj, OptPairTensor, Size
+from torch_geometric.typing import Adj, OptTensor, PairTensor
+from torch_geometric.utils import softmax
 
 
 class PeriodConv(MessagePassing):
-    r"""The GraphSAGE operator from the `"Inductive Representation Learning on
-    Large Graphs" <https://arxiv.org/abs/1706.02216>`_ paper
+    r"""The graph transformer operator from the `"Masked Label Prediction:
+    Unified Message Passing Model for Semi-Supervised Classification"
+    <https://arxiv.org/abs/2009.03509>`_ paper
+
     .. math::
-        \mathbf{x}^{\prime}_i = \mathbf{W}_1 \mathbf{x}_i + \mathbf{W}_2 \cdot
-        \mathrm{mean}_{j \in \mathcal{N(i)}} \mathbf{x}_j
-    If :obj:`project = True`, then :math:`\mathbf{x}_j` will first get
-    projected via
+        \mathbf{x}^{\prime}_i = \mathbf{W}_1 \mathbf{x}_i +
+        \sum_{j \in \mathcal{N}(i)} \alpha_{i,j} \mathbf{W}_2 \mathbf{x}_{j},
+
+    where the attention coefficients :math:`\alpha_{i,j}` are computed via
+    multi-head dot product attention:
+
     .. math::
-        \mathbf{x}_j \leftarrow \sigma ( \mathbf{W}_3 \mathbf{x}_j +
-        \mathbf{b})
-    as described in Eq. (3) of the paper.
+        \alpha_{i,j} = \textrm{softmax} \left(
+        \frac{(\mathbf{W}_3\mathbf{x}_i)^{\top} (\mathbf{W}_4\mathbf{x}_j)}
+        {\sqrt{d}} \right)
+
     Args:
         in_channels (int or tuple): Size of each input sample, or :obj:`-1` to
             derive the size from the first input(s) to the forward method.
             A tuple corresponds to the sizes of source and target
             dimensionalities.
         out_channels (int): Size of each output sample.
-        aggr (string or Aggregation, optional): The aggregation scheme to use.
-            Any aggregation of :obj:`torch_geometric.nn.aggr` can be used,
-            *e.g.*, :obj:`"mean"`, :obj:`"max"`, or :obj:`"lstm"`.
-            (default: :obj:`"mean"`)
-        normalize (bool, optional): If set to :obj:`True`, output features
-            will be :math:`\ell_2`-normalized, *i.e.*,
-            :math:`\frac{\mathbf{x}^{\prime}_i}
-            {\| \mathbf{x}^{\prime}_i \|_2}`.
-            (default: :obj:`False`)
-        root_weight (bool, optional): If set to :obj:`False`, the layer will
-            not add transformed root node features to the output.
+        heads (int, optional): Number of multi-head-attentions.
+            (default: :obj:`1`)
+        concat (bool, optional): If set to :obj:`False`, the multi-head
+            attentions are averaged instead of concatenated.
             (default: :obj:`True`)
-        project (bool, optional): If set to :obj:`True`, the layer will apply a
-            linear transformation followed by an activation function before
-            aggregation (as described in Eq. (3) of the paper).
-            (default: :obj:`False`)
+        beta (bool, optional): If set, will combine aggregation and
+            skip information via
+
+            .. math::
+                \mathbf{x}^{\prime}_i = \beta_i \mathbf{W}_1 \mathbf{x}_i +
+                (1 - \beta_i) \underbrace{\left(\sum_{j \in \mathcal{N}(i)}
+                \alpha_{i,j} \mathbf{W}_2 \vec{x}_j \right)}_{=\mathbf{m}_i}
+
+            with :math:`\beta_i = \textrm{sigmoid}(\mathbf{w}_5^{\top}
+            [ \mathbf{W}_1 \mathbf{x}_i, \mathbf{m}_i, \mathbf{W}_1
+            \mathbf{x}_i - \mathbf{m}_i ])` (default: :obj:`False`)
+        dropout (float, optional): Dropout probability of the normalized
+            attention coefficients which exposes each node to a stochastically
+            sampled neighborhood during training. (default: :obj:`0`)
+        edge_dim (int, optional): Edge feature dimensionality (in case
+            there are any). Edge features are added to the keys after
+            linear transformation, that is, prior to computing the
+            attention dot product. They are also added to final values
+            after the same linear transformation. The model is:
+
+            .. math::
+                \mathbf{x}^{\prime}_i = \mathbf{W}_1 \mathbf{x}_i +
+                \sum_{j \in \mathcal{N}(i)} \alpha_{i,j} \left(
+                \mathbf{W}_2 \mathbf{x}_{j} + \mathbf{W}_6 \mathbf{e}_{ij}
+                \right),
+
+            where the attention coefficients :math:`\alpha_{i,j}` are now
+            computed via:
+
+            .. math::
+                \alpha_{i,j} = \textrm{softmax} \left(
+                \frac{(\mathbf{W}_3\mathbf{x}_i)^{\top}
+                (\mathbf{W}_4\mathbf{x}_j + \mathbf{W}_6 \mathbf{e}_{ij})}
+                {\sqrt{d}} \right)
+
+            (default :obj:`None`)
         bias (bool, optional): If set to :obj:`False`, the layer will not learn
             an additive bias. (default: :obj:`True`)
+        root_weight (bool, optional): If set to :obj:`False`, the layer will
+            not add the transformed root node features to the output and the
+            option  :attr:`beta` is set to :obj:`False`. (default: :obj:`True`)
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
-    Shapes:
-        - **inputs:**
-          node features :math:`(|\mathcal{V}|, F_{in})` or
-          :math:`((|\mathcal{V_s}|, F_{s}), (|\mathcal{V_t}|, F_{t}))`
-          if bipartite,
-          edge indices :math:`(2, |\mathcal{E}|)`
-        - **outputs:** node features :math:`(|\mathcal{V}|, F_{out})` or
-          :math:`(|\mathcal{V_t}|, F_{out})` if bipartite
     """
+    _alpha: OptTensor
+
     def __init__(
         self,
         in_channels: Union[int, Tuple[int, int]],
         out_channels: int,
-        aggr: Optional[Union[str, List[str], Aggregation]] = "mean",
-        normalize: bool = False,
-        root_weight: bool = True,
-        project: bool = False,
+        heads: int = 1,
+        concat: bool = True,
+        beta: bool = False,
+        dropout: float = 0.,
+        edge_dim: Optional[int] = None,
         bias: bool = True,
+        root_weight: bool = True,
         **kwargs,
     ):
+        kwargs.setdefault('aggr', 'add')
+        super().__init__(node_dim=0, **kwargs)
+       # if edge_dim==0: edge_dim=None
+        edge_dim = 1
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.normalize = normalize
+        self.heads = heads
+        self.beta = beta and root_weight
         self.root_weight = root_weight
-        self.project = project
+        self.concat = concat
+        self.dropout = dropout
+        self.edge_dim = edge_dim
+        self._alpha = None
 
         if isinstance(in_channels, int):
             in_channels = (in_channels, in_channels)
 
-        if aggr == 'lstm':
-            kwargs.setdefault('aggr_kwargs', {})
-            kwargs['aggr_kwargs'].setdefault('in_channels', in_channels[0])
-            kwargs['aggr_kwargs'].setdefault('out_channels', in_channels[0])
-
-        super().__init__(aggr, **kwargs)
-
-        if self.project:
-            self.lin = Linear(in_channels[0], in_channels[0], bias=True)
-
-        if self.aggr is None:
-            self.fuse = False  # No "fused" message_and_aggregate.
-            self.lstm = LSTM(in_channels[0], in_channels[0], batch_first=True)
-
-        if isinstance(self.aggr_module, MultiAggregation):
-            aggr_out_channels = self.aggr_module.get_out_channels(
-                in_channels[0])
+        self.lin_key = Linear(in_channels[0], heads * out_channels)
+        self.lin_query = Linear(in_channels[1], heads * out_channels)
+        self.lin_value = Linear(in_channels[0], heads * out_channels)
+        self.lin_l2 = Linear(heads * out_channels, heads * out_channels, bias=bias)
+        
+        if edge_dim is not None:
+            self.lin_edge = Linear(edge_dim, heads * out_channels, bias=False)
         else:
-            aggr_out_channels = in_channels[0]
+            self.lin_edge = self.register_parameter('lin_edge', None)
 
-        self.lin_l = Linear(aggr_out_channels, out_channels, bias=bias)
-        self.lin_l2 = Linear(out_channels, out_channels, bias=bias)
-        if self.root_weight:
-            self.lin_r = Linear(in_channels[1], out_channels, bias=False)
+        if concat:
+            self.lin_skip = Linear(in_channels[1], heads * out_channels,
+                                   bias=bias)
+            if self.beta:
+                self.lin_beta = Linear(3 * heads * out_channels, 1, bias=False)
+            else:
+                self.lin_beta = self.register_parameter('lin_beta', None)
+        else:
+            self.lin_skip = Linear(in_channels[1], out_channels, bias=bias)
+            if self.beta:
+                self.lin_beta = Linear(3 * out_channels, 1, bias=False)
+            else:
+                self.lin_beta = self.register_parameter('lin_beta', None)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        if self.project:
-            self.lin.reset_parameters()
-        self.aggr_module.reset_parameters()
-        self.lin_l.reset_parameters()
+        self.lin_key.reset_parameters()
+        self.lin_query.reset_parameters()
+        self.lin_value.reset_parameters()
         self.lin_l2.reset_parameters()
-        if self.root_weight:
-            self.lin_r.reset_parameters()
+        if self.edge_dim:
+            self.lin_edge.reset_parameters()
+        self.lin_skip.reset_parameters()
+        if self.beta:
+            self.lin_beta.reset_parameters()
 
-    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
-                size: Size = None) -> Tensor:
-        """"""
-       # print('tuple', x[0].shape, x[1].shape)
+
+    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None, return_attention_weights=None):
+
+        r"""
+        Args:
+            return_attention_weights (bool, optional): If set to :obj:`True`,
+                will additionally return the tuple
+                :obj:`(edge_index, attention_weights)`, holding the computed
+                attention weights for each edge. (default: :obj:`None`)
+        """
+
+
+
         if isinstance(x, Tensor):
-       #     print(x.shape)
-            x: OptPairTensor = (x, x)
+            x: PairTensor = (x, x)
 
-        if self.project and hasattr(self, 'lin'):
-            x = (self.lin(x[0]).relu(), x[1])
+        # propagate_type: (query: Tensor, key:Tensor, value: Tensor, edge_attr: OptTensor) # noqa
+        out = self.propagate(edge_index, x=x,
+                             edge_attr=edge_attr, size=None)
 
-        # propagate_type: (x: OptPairTensor)
-        out = self.propagate(edge_index, x=x, size=size)
-     #   out = self.lin_l(out)
+        alpha = self._alpha
+        self._alpha = None
 
-        x_r = x[1]
-        if self.root_weight and x_r is not None:
-            out = out + self.lin_r(x_r)
+        if self.concat:
+            out = out.view(-1, self.heads * self.out_channels)
+        else:
+            out = out.mean(dim=1)
 
-        if self.normalize:
-            out = F.normalize(out, p=2., dim=-1)
+        if self.root_weight:
+            x_r = self.lin_skip(x[1])
+            if self.lin_beta is not None:
+                beta = self.lin_beta(torch.cat([out, x_r, out - x_r], dim=-1))
+                beta = beta.sigmoid()
+                out = beta * x_r + (1 - beta) * out
+            else:
+                out = out + x_r
 
-        return out
+        if isinstance(return_attention_weights, bool):
+            assert alpha is not None
+            if isinstance(edge_index, Tensor):
+                return out, (edge_index, alpha)
+            elif isinstance(edge_index, SparseTensor):
+                return out, edge_index.set_value(alpha, layout='coo')
+        else:
+            return out
 
-    def message(self, x_i: Tensor, x_j: Tensor) -> Tensor:
+
+    def message(self, x_j: Tensor, x_i: Tensor, 
+                edge_attr: OptTensor, index: Tensor, ptr: OptTensor,
+                size_i: Optional[int]) -> Tensor:
         
+        H, C = self.heads, self.out_channels
         rel_loc = x_j[:,:3] - x_i[:,:3]
-      #  print(-1*(rel_loc>0.5))
         reloc = -1*(rel_loc>0.5) + 1*(rel_loc<-0.5) + rel_loc
+        x_j = torch.cat([reloc, x_j[:,3:]], dim=1)
         
-      #  print(reloc)
+    #    return self.lin_l2(F.relu(self.lin_l( torch.cat([reloc, x_j[:,3:]], dim=1) )))
+    
+    
+        query = self.lin_query(x_i).view(-1, H, C)
+        key = self.lin_key(x_j).view(-1, H, C)
+        value = self.lin_l2(F.relu(self.lin_value(x_j).view(-1, H, C)))
         
-        return self.lin_l2(F.relu(self.lin_l( torch.cat([reloc, x_j[:,3:]], dim=1) )))
-    
-    
-    
+        if self.lin_edge is not None:
+            assert edge_attr is not None
+            edge_attr = self.lin_edge(edge_attr).view(-1, self.heads,
+                                                      self.out_channels)
+            key = key + edge_attr
+
+        alpha = (query * key).sum(dim=-1) / math.sqrt(self.out_channels)
+        alpha = softmax(alpha, index, ptr, size_i)
+        self._alpha = alpha
+        alpha = F.dropout(alpha, p=self.dropout, training=self.training)
+
+        out = value
+        if edge_attr is not None:
+            out = out + edge_attr
+
+        #out = out * alpha.view(-1, self.heads, 1)
+        return out
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}({self.in_channels}, '
-                f'{self.out_channels}, aggr={self.aggr})')
+                f'{self.out_channels}, heads={self.heads})')
